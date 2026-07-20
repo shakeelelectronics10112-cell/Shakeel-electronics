cf08782e7913243add8a6ede9594ab11aa7d80a4
shakeelelectronics10112-cell <shakeelelectronics10112@gmail.com>
2026-07-20 17:43:56 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Shakeel Electronics — offline service worker */
-const CACHE = "shakeel-v21";
+const CACHE = "shakeel-v23";
 const ASSETS = ["./","./index.html","./ledger.html","./stockbox.html","./orderbook.html","./purchases.html","./labels.html","./quotes.html","./admin.html"];
 
 self.addEventListener("install", (e) => {
